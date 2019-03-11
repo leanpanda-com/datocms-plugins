9e9d058fb0d4439fdf5e7432ed7662255ac967af
Irene <i.oppo@cantierecreativo.net>
2019-03-11 15:36:26 +0100

    hide field from role

--- hide-field-from-role/src/index.js
+++ hide-field-from-role/src/index.js
@@ -1,15 +1,24 @@
-window.DatoCmsPlugin.init((plugin) => {
-  plugin.startAutoResizer();
+import { SiteClient } from 'datocms-client';
 
-  let path;
+function toggleField(roleName, plugin) {
+  const hideFromRoles = plugin.parameters.instance.roles.split(',')
+    .map(r => r.toLowerCase());
 
-  if (plugin.field.attributes.localized) {
-    path = `${plugin.fieldPath}.${plugin.locale}`;
-  } else {
-    path = plugin.fieldPath;
-  }
+  if (hideFromRoles.includes(roleName.toLowerCase())) {
+    let path;
 
-  if (plugin.currentUser) {
+    if (plugin.field.attributes.localized) {
+      path = `${plugin.fieldPath}.${plugin.locale}`;
+    } else {
+      path = plugin.fieldPath;
+    }
     plugin.toggleField(path, false);
   }
+}
+
+window.DatoCmsPlugin.init((plugin) => {
+  const dato = new SiteClient(plugin.parameters.global.apiToken);
+  dato.roles.find(plugin.currentUser.relationships.role.data.id)
+    .then(role => toggleField(role.name, plugin))
+    .catch(e => console.log(e));
 });
