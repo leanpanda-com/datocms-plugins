7822956841e2d8b30a0ed314c2a7409367eb424a
Irene <i.oppo@cantierecreativo.net>
2019-03-19 15:34:55 +0100

    ready to go image alt title, ignored all incoplete plugins

--- set-image-alt-title/src/index.js
+++ set-image-alt-title/src/index.js
@@ -6,17 +6,22 @@ function editImages(plugin, button) {
   const dato = new SiteClient(plugin.parameters.global.apiToken);
   const altValue = document.getElementById('alt').value;
   const titleValue = document.getElementById('title').value;
-  console.log(altValue, titleValue);
 
-  const imageFields = Object.values(plugin.fields)
+  const imageFieldsIds = Object.values(plugin.fields)
     .filter(f => ['file', 'gallery'].includes(f.attributes.field_type)
       && f.relationships.item_type.data.id === plugin.itemType.id)
     .map(field => field.attributes.api_key);
 
-  dato.item.find(plugin.itemId)
+  dato.item.find(plugin.itemId, { version: 'current' })
     .then((item) => {
-      const uploadIds = imageFields.map(imageField => item[imageField]);
-      console.log(uploadIds);
+      const uploadIds = imageFieldsIds.map((imageFieldValue) => {
+        if (typeof item[imageFieldValue] === 'object') {
+          return item[imageFieldValue][plugin.locale];
+        }
+
+        return item[imageFieldValue];
+      }).flat();
+
       uploadIds.forEach((id) => {
         dato.upload.find(id)
           .then((u) => {
@@ -24,21 +29,23 @@ function editImages(plugin, button) {
               dato.upload.update(id, {
                 alt: altValue,
                 title: titleValue,
-              }).then(res => console.log(res));
+              });
             }
           });
       });
     })
     .then(() => {
+      /* eslint-disable */
       button.disabled = false;
       button.classList.remove('loading');
       button.classList.add('done');
+      /* eslint-enable */
     })
     .catch((error) => {
+      /* eslint-disable */
       button.disabled = false;
       button.classList.remove('loading');
       button.classList.add('error');
-      /* eslint-disable */
       alert(error);
       /* eslint-enable */
     });
@@ -46,15 +53,20 @@ function editImages(plugin, button) {
 
 window.DatoCmsPlugin.init((plugin) => {
   plugin.startAutoResizer();
-  const { field, locale } = plugin;
+  const { locale, site } = plugin;
 
   const container = document.createElement('div');
   container.classList.add('container');
 
+  const hint = document.createElement('p');
+  hint.innerHTML = `By clicking on Apply you will edit all alts and titles of images in this record. Refresh to see the updates${site.attributes.locales.length > 1 ? `. This action will only affect only the "${locale}" locale, localize this field to be able to edit also other locales` : '.'}`;
+  hint.classList.add('hint');
+  container.appendChild(hint);
+
   const altInput = document.createElement('input');
   altInput.classList.add('alt');
   altInput.id = 'alt';
-  const altLabel = document.createElement('Label');
+  const altLabel = document.createElement('label');
   altLabel.htmlFor = 'alt';
   altLabel.innerHTML = 'Alt';
   container.appendChild(altLabel);
@@ -63,7 +75,7 @@ window.DatoCmsPlugin.init((plugin) => {
   const titleInput = document.createElement('input');
   titleInput.classList.add('alt');
   titleInput.id = 'title';
-  const titleLabel = document.createElement('Label');
+  const titleLabel = document.createElement('label');
   titleLabel.htmlFor = 'title';
   titleLabel.innerHTML = 'Title';
   container.appendChild(titleLabel);
@@ -73,7 +85,7 @@ window.DatoCmsPlugin.init((plugin) => {
   const spinner = document.createElement('span');
 
   button.id = ('DatoCMS-button--primary');
-  button.textContent = `Apply to all ${field.attributes.localized ? `(${locale})` : ''}`;
+  button.textContent = `Apply (${site.attributes.locales.length > 1 ? `${locale})` : ''}`;
   button.appendChild(spinner);
   spinner.id = ('spinner');
   container.appendChild(button);
@@ -83,7 +95,7 @@ window.DatoCmsPlugin.init((plugin) => {
     event.preventDefault();
 
     /* eslint-disable */
-    const confirm = window.confirm(`This action will overwrite all previous content for the '${field.attributes.label}' field belonging to all records of the ${plugin.itemType.attributes.name} model. Are you sure you want to proceed?`);
+    const confirm = window.confirm(`This action will overwrite all titles and alts belonging to images of this record. Are you sure you want to proceed?`);
     /* eslint-enable */
     if (!confirm) {
       return;
