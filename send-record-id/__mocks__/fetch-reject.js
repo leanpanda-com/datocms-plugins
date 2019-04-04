ba33162748eb0a4f266f91f07e2423a6f083a900
Irene <i.oppo@cantierecreativo.net>
2019-04-04 11:24:01 +0200

    prove con fetch reject, senza results

--- send-record-id/__mocks__/fetch-reject.js
+++ send-record-id/__mocks__/fetch-reject.js
@@ -1,3 +1,11 @@
-const fetch = jest.fn((url, options) => Promise.resolve({status: 200}));
+const fetch = jest.fn((url, options) =>
+  new Promise((resolve, reject) => {
+    if (url !== 'invalid.url') {
+      return resolve({ status: 200 });
+    } else {
+      return reject({ status: 500 });
+    }
+  })
+);
 
 export default fetch;
--- send-record-id/src/sendRecordId.test.js
+++ send-record-id/src/sendRecordId.test.js
@@ -34,35 +34,39 @@ describe('sendRecordId', () => {
 
   it('starts the auto resizer', () => {
     const plugin = mockPluginFactory();
-
     sendRecordId(plugin, document, window);
-
     expect(plugin.startAutoResizer).toHaveBeenCalledTimes(1);
   });
 
-  it('calls fetch with itemID when button is clicked', async () => {
-    sendRecordId(mockPluginFactory(), document, window);
-    const button = document.getElementById('DatoCMS-button--primary');
-    button.click();
-    expect(button.classList.contains('loading')).toBe(true);
-    await flushPromises();
-    const call = fetch.mock.calls[0];
-    expect(fetch).toHaveBeenCalledTimes(1);
-    expect(call[0]).toBe('http://test.com');
-    expect(call[1].body).toBe(JSON.stringify({id: "123"}));
-    expect(button.classList.contains('loading')).toBe(false);
-    expect(statement.textContent).toBe('Request made to http://test.com with body {"id":"123"} - success!');
+  describe('when request succeeds', () => {
+    it('calls fetch with itemID when button is clicked', async () => {
+      sendRecordId(mockPluginFactory(), document, window);
+      const button = document.getElementById('DatoCMS-button--primary');
+      button.click();
+      expect(button.classList.contains('loading')).toBe(true);
+      await flushPromises();
+      const call = fetch.mock.calls[0];
+      expect(fetch).toHaveBeenCalledTimes(1);
+      expect(call[0]).toBe('http://test.com');
+      expect(call[1].body).toBe(JSON.stringify({id: "123"}));
+      expect(button.classList.contains('loading')).toBe(false);
+      expect(fetch(call[0], call[1].body)).resolves.toEqual({"status": 200});
+      expect(statement.textContent).toBe('Request made to http://test.com with body {"id":"123"} - success!');
+    });
   });
 
-  it('resets button if failed call', async () => {
-    sendRecordId(mockPluginFactory(), document, window);
-    const button = document.getElementById('DatoCMS-button--primary');
-    button.click();
-    expect(button.classList.contains('loading')).toBe(true);
-    await flushPromises();
-    const statement = document.getElementById('statement');
-    console.log(statement.textContent);
-    // expect(statement.textContent).toBe('500');
-    expect(button.classList.contains('loading')).toBe(false);
+  describe('when request fails', () => {
+    it('resets button if failed call', async () => {
+      sendRecordId(mockPluginFactory(), document, window);
+      const button = document.getElementById('DatoCMS-button--primary');
+      button.click();
+      expect(button.classList.contains('loading')).toBe(true);
+      await flushPromises();
+      const statement = document.getElementById('statement');
+      console.log(statement.textContent);
+      expect(fetch('invalid.url')).rejects.toEqual({"status": 500});
+      // expect(statement.textContent).toBe('500');
+      expect(button.classList.contains('loading')).toBe(false);
+    });
   });
 })
