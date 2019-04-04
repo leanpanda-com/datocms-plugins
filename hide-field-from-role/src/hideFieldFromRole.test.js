cb0865d7220b01a258347d6bb13fc11366d79929
Irene <i.oppo@cantierecreativo.net>
2019-04-04 12:18:57 +0200

    tests role discrimination

--- hide-field-from-role/src/hideFieldFromRole.test.js
+++ hide-field-from-role/src/hideFieldFromRole.test.js
@@ -4,64 +4,62 @@ import flushPromises from './test/support/flushPromises'
 jest.mock('datocms-client')
 
 const mockPluginFactory = (options = {}) => {
-  const defaults = {
-    id: '345',
-    fieldPath: 'foo',
-    localized: false,
-    singleton: false,
-    unique: false
-  }
-
-  const opts = Object.assign(defaults, options)
-
   return {
     parameters: {
       global: {
         api_key: 'ciao'
       },
       instance: {
-        roles: 'editor,translator'
+        roles: 'editor, translator'
       },
     },
     startAutoResizer: jest.fn(() => null),
     toggleField: jest.fn(() => null),
     getFieldValue: jest.fn(() => 'value'),
-    currentUser: {relationships: {role: { data: { id: opts.id}}}},
+    currentUser: {relationships: {role: { data: { id: '345'}}}},
     field: {
       attributes: {
-        label: 'title',
-        localized: opts.localized,
+        label: 'title'
       }
     },
-    fieldPath: opts.fieldPath,
-    locale: opts.locale
+    fieldPath: 'title'
   }
 }
 
 describe('hideFieldFromRole', () => {
-  beforeEach(() => {
-    roles.find = jest.fn(() => Promise.resolve({
-      id: '12',
-      name: 'editor',
-    }))
-  })
-
   afterEach(() => {
     document.getElementsByTagName('html')[0].innerHTML = ''
   })
 
-  it('starts the auto resizer', () => {
-    const plugin = mockPluginFactory()
+  describe('if currentUser is on the list', () => {
+    beforeAll(() => {
+      roles.find = jest.fn(() => Promise.resolve({
+        name: 'editor',
+      }))
+    })
 
-    hideFieldFromRole(plugin, document, window)
-
-    expect(plugin.startAutoResizer).toHaveBeenCalledTimes(1)
+    it('hides field', async () => {
+      const plugin = mockPluginFactory()
+      hideFieldFromRole(plugin, document, window)
+      await flushPromises()
+      expect(roles.find).toHaveBeenCalledTimes(1)
+      expect(plugin.toggleField).toHaveBeenCalledTimes(1)
+    })
   })
 
-  it('hides field from editor but shows it to admin', async () => {
-    hideFieldFromRole(mockPluginFactory(), document, window)
+  describe('if currentUser is not on the list', () => {
+    beforeAll(() => {
+      roles.find = jest.fn(() => Promise.resolve({
+        name: 'admin',
+      }))
+    })
 
-    await flushPromises()
-    expect(roles.find).toHaveBeenCalledTimes(1)
+    it('does not hide field', async () => {
+      const plugin = mockPluginFactory()
+      hideFieldFromRole(plugin, document, window)
+      await flushPromises()
+      expect(roles.find).toHaveBeenCalledTimes(1)
+      expect(plugin.toggleField).toHaveBeenCalledTimes(0)
+    })
   })
 })
